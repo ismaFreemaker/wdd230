6829b2f3130666a38abbb39cf892fef73fd19732
ismaFreemaker <rojasisma10@gmail.com>
2022-04-25 21:16:04 -0300

    added more js

---
+++ w1/js/dinamic-index.js
@@ -0,0 +1,19 @@
+let nLastModified = new Date(document.lastModified);
+
+let day = nLastModified.getDay();
+let month = nLastModified.getMonth();
+let year = nLastModified.getFullYear();
+let hour = nLastModified.getHours();
+let minutes = nLastModified.getMinutes();
+let seconds = nLastModified.getSeconds();
+
+let updatedDateParagraphSpan = document.querySelector(".last-update");
+
+updatedDateParagraphSpan.textContent = `The last day that was modified was: ${day}/${month}/${year}  ${hour}:${minutes}:${seconds}`;
+// updatedDateParagraphSpan.textContent = `${nLastModified}`;
+
+let copyrightParagraph = document.querySelector("#copyright-parag");
+copyrightParagraph.innerHTML = `&copy 2022 - R. Ismael Rojas - Argentina`;
+
+
+
