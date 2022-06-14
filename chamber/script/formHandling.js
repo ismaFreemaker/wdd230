4d028ba13a0c20ce9f626a66ba3b097507fa7219
ismaFreemaker <rojasisma10@gmail.com>
2022-06-14 12:53:17 -0300

    finished w8 activity in chamber

--- chamber/script/formHandling.js
+++ chamber/script/formHandling.js
@@ -8,21 +8,49 @@ const businessPosition = document.querySelector('input[name="busposition"]');
 const latinCharacter = /([\w])/;
 const spaceCharacter = /([\s])/;
 const hyphenCharacter = /([-])/;
+const alphaCharacters = /[A-aZ-z]/; 
+const numberCharacters = /[0-9]/;
+const permittedCharacters = /([0-9])|([^A-aZ-z\s-])/;
+const avoidSymbols = /[\*\"\!\#\$\%\&\/\(\)\=\?\'\"\+\.\,\:\;0-9]/;
 
 businessPosition.addEventListener("focusout", checkJob);
 
+const re = new RegExp();
+
+function checkCharacter(){
+
+}
 
 
 function checkJob () {
     // conditional for the matches of the business input box
-    if ((businessPosition.value.match(latinCharacter) || businessPosition.value.match(hyphenCharacter) || businessPosition.value.match(spaceCharacter)) && (businessPosition.value.length >= 7)){
-        businessPosition.style.backgroundColor = 'green';
-    } else {
-        businessPosition.style.backgroundColor = 'rgba(200, 0, 0, 0.7)';   
+    let warningMessage = document.querySelector(".warning-box");
+    if (businessPosition.value.length === 0){
+        document.querySelector(".warning-box").style.display = "none";
+        businessPosition.style.backgroundColor = 'white';   
+    }
+    else if (businessPosition.value.match(permittedCharacters) ){
+        businessPosition.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+        warningMessage.textContent = "Only alpha characters, spaces and '-' allowed. Not numbers or other symbols";
+        warningMessage.style.display = "block";
 
+    }else if ((businessPosition.value.length <= 7)){
+        businessPosition.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+        warningMessage.textContent = "The filed must be at least 7 characters.";
+        warningMessage.style.display = "block";
+    }else{
+        document.querySelector(".warning-box").style.display = "none";
+        businessPosition.style.backgroundColor = 'white';   
     }
 }
 
+// down here the script for the hidden input
+var thisDay = new Date();
+var dd = thisDay.getDate();
+var mm = thisDay.getMonth() + 1;
+var yyyy = thisDay.getFullYear();
+
+document.querySelector("#daySubmitted").value = dd + "/" + mm + "/" + yyyy;
 
 
 
