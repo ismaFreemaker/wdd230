b0e959e8df14780ca699b843611dc55ebd626070
ismaFreemaker <rojasisma10@gmail.com>
2022-06-18 20:47:05 -0300

    modified json

--- chamber/script/enterprises-data.js
+++ chamber/script/enterprises-data.js
@@ -14,13 +14,15 @@ async function getEnterprises(){
 function builtEnterpriseCard(data){
     data.enterprises.forEach(enterprise => {
         let card = document.createElement('section');        
-        let h2Name = document.createElement('h2');        
+        let h3Name = document.createElement('h3');        
         let brand = document.createElement('img');        
         let pAddress = document.createElement('p');        
         let pPhoneNumber = document.createElement('p');        
         let website = document.createElement('a');   
-        
-        h2Name.textContent = `${enterprise.name}`;
+        let membershipLevel = document.createElement('p');
+
+
+        h3Name.textContent = `${enterprise.name}`;
         brand.setAttribute('src', `${enterprise.imagelink}`);
         brand.setAttribute('alt', `${enterprise.name} logo`);
         brand.setAttribute('loading', 'lazy');
@@ -28,15 +30,22 @@ function builtEnterpriseCard(data){
         pPhoneNumber.textContent = `${enterprise.phonenumber}`;
         website.textContent = `${enterprise.website}`;
         website.setAttribute('href', `${enterprise.website}`);
-        
+        membershipLevel.textContent = `${enterprise.membershiplevel} Membership`;        
 
-        card.append(h2Name);
+        if (enterprise.membershiplevel === "Gold"){
+            membershipLevel.classList.add("gold-membership-level");
+        }else if(enterprise.membershiplevel === "Silver"){
+            membershipLevel.classList.add("silver-membership-level");
+            
+        }else if(enterprise.membershiplevel === "Bronze"){
+            membershipLevel.classList.add("bronze-membership-level");
+        }
+        card.append(h3Name);
         card.append(brand);
         card.append(pAddress);
         card.append(pPhoneNumber);
         card.append(website);
-        
-
+        card.append(membershipLevel);
 
         cards.append(card);
     });
@@ -44,15 +53,20 @@ function builtEnterpriseCard(data){
 
 getEnterprises();
 
+// making the code for the cards view
 
+let gridButton = document.querySelector('#grid-button');
+let listButton = document.querySelector('#list-button');
 
+gridButton.addEventListener("click", () =>{
+    cards.classList.add('grid');
+    cards.classList.remove('list');
+});
 
-
-
-
-
-
-
+listButton.addEventListener("click", () => {
+    cards.classList.add("list");
+    cards.classList.remove("grid");
+});
 
 
 
