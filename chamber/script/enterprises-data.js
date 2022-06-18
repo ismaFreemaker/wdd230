814722e0dcca63a2086dbd36d0bc771e6697bb58
ismaFreemaker <rojasisma10@gmail.com>
2022-06-17 22:10:04 -0300

    adding the data.json

---
+++ chamber/script/enterprises-data.js
@@ -0,0 +1,55 @@
+const requestUrl = "https://ismafreemaker.github.io/wdd230/chamber/data.json";
+const cards = document.querySelector(".cards");
+
+async function getEnterprises(){
+    let response = await fetch(requestUrl);
+    if (response.ok){
+        let data = await response.json();
+        builtEnterpriseCard(data);
+    }  else{
+        throw Error(response.statusText);
+    } 
+}
+
+function builtEnterpriseCard(data){
+    data.enterprises.forEach(enterprise => {
+        let card = document.createElement('section');        
+        let h2Name = document.createElement('h2');        
+        let brand = document.createElement('img');        
+        let pAddress = document.createElement('p');        
+        let pPhoneNumber = document.createElement('p');        
+        let website = document.createElement('a');   
+        
+        h2Name.textContent = `${enterprise.name}`;
+        brand.setAttribute('src', `${enterprise.imagelink}`)
+
+
+        card.append(h2Name);
+        card.append(brand);
+
+
+        cards.append(card);
+    });
+}
+
+getEnterprises();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
